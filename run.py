63597f3b505fd25ce0df9744f8aa454ae7931f61
Hijae song <hijae99@gmail.com>
2020-12-14 00:04:54 +0900

    Update run.py
    한번에 여러 공지사항이 올라왔을때 하나만 올라오던 문제 해결

--- run.py
+++ run.py
@@ -14,36 +14,38 @@ import telegram
 
 
 
-bot = telegram.Bot(token='yourtoken')
+bot = telegram.Bot(token='youtoken')
 
 
 
 if __name__ == '__main__':
 
-    # 제일 최신 게시글의 번호 저장
-    latest_num = 0
+    # 제일 최신 게시글의 이름 저장
+    latest_name = []
     while True:
         req = requests.get('http://www.cu.ac.kr/plaza/notice/notice')
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
         posts = soup.find("tbody")
-        for link in posts.find_all('td'):
-            post_num=link.text.strip()
-            break
+        post_name=[]
+        postlink=[]
         for link in posts.find_all('a'):
-            post_name=link.text.strip()
-            postlink=link.get('href')
-            break
-
-        # 제일 최신 게시글 번호와 180초 마다 크롤링한 첫번째 게시글의 번호 비교
+            if link.get('href')!="#pdsWinOpen" and link.get('title')!="첨부파일 다운로드": #첨부파일, 다중첨부파일 무시
+                post_name.append(link.text.strip())
+                postlink.append(link.get('href'))
+        nametemp = list(set(post_name) - set(latest_name))
+        if len(nametemp)>= 1 :
+            latest_name = post_name
+            for i in nametemp:
+                links = 'http://www.cu.ac.kr'+postlink[post_name.index(i)]
+                text = '<새 공지사항>'+'\n'+i+'\n[inline URL]('+str(links)+')'
+                bot.sendMessage(-1001257279942, text)
+                time.sleep(5)
+                # 프롬프트 로그
+                print(text)
+        
+        # 제일 최신 게시글 번호와 300초 마다 크롤링한 첫번째 게시글의 이름 비교
         # 비교 후 같지 않으면 최신 게시글 업데이트 된 것으로 텔레그램 봇으로 업데이트 메시지 전송
-        if latest_num != post_num :
-            latest_num = post_num
-            links = 'http://www.cu.ac.kr/'+postlink
-
-            text = '<대구가톨릭대학교 공지사항 업데이트>'+'\n'+post_num+'\n'+post_name+'\n'+links
-            #bot.sendMessage(-1001257279942, text)
-            # 프롬프트 로그
-            print(text)
-        time.sleep(180) # 180초(3분) 간격으로 크롤링
-        print('bot 동작 중 현재 게시글 번호' + latest_num)
+        
+        time.sleep(300-len(nametemp)*5) # 300초(5분) 간격으로 크롤링
+        print('bot 동작 중 현재', post_name)
